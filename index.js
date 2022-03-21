1960b3ec098011c73edfa4648cb30891cfa88d23
Daniel Brown <danielbrown25.9.2@gmail.com>
2022-03-21 20:46:18 -0300

    testing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   console.log("Works", process.env.ENV_TEST);
-  res.send("Hello World! env_status:" + process.env.ENV_TEST);
+  res.send("AEEE POHAAA! env_status:" + process.env.ENV_TEST);
 });
 
 app.listen(3000, () => {
